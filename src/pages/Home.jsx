c6913617b83ef9613e37111fe383bd6190837fb5
valentinacamacho <valentinacamacho0206@gmail.com>
2025-05-12 20:08:21 -0500

    We place the product structure on the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,14 @@
-function Home(){
+function Home() {
     return (
-        <h1 className="text-center mt-4">Página de inicio - Productos</h1>
+        <div className="container mt-4">
+            <h2 className="text-center mb-4">Todos los Productos</h2>
+            <div className="card-body">
+                <h5 className="card-title">Nombre del producto</h5>
+                <p className="card-text">precio del producto</p>
+            </div>
+        </div>
+
+
     )
 };
 
