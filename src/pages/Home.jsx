81e43183d204c5257fb48ca3e7ac4a5a43f4601b
valentinacamacho <valentinacamacho0206@gmail.com>
2025-05-12 20:15:15 -0500

    Initialize the hooks to be used in pages/home.jsx where the product API connection is made

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,33 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function Home() {
-    return (
-        <div className="container mt-4">
-            <h2 className="text-center mb-4">Todos los Productos</h2>
-            <div className="card-body">
-                <h5 className="card-title">Nombre del producto</h5>
-                <p className="card-text">precio del producto</p>
-            </div>
-        </div>
+  const [productos, setProductos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products')
+      .then((res) => {
+        setProductos(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error al obtener productos:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h2 className="text-center mb-4">Todos los Productos</h2>
+      <div className="card-body">
+        <h5 className="card-title">Nombre del producto</h5>
+        <p className="card-text">precio del producto</p>
+      </div>
+    </div>
 
 
-    )
+  )
 };
 
 export default Home;
